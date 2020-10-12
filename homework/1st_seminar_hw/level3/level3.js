019fc54b6d523376ecd05029904dce7ecbb42e52
yxxshin <samshin3910@snu.ac.kr>
2020-10-13 08:15:24 +0900

    Finished 1st_seminar_hw_level3

---
+++ homework/1st_seminar_hw/level3/level3.js
@@ -0,0 +1,36 @@
+const members = require('./members');
+
+// filter members
+const OB_members = members.filter(i => i.status === 'OB');
+const YB_members = members.filter(i => i.status === 'YB');
+
+// Randomize each arrays
+const Randomize = memArr => {
+    for(var i = 0; i < memArr.length; i++) {
+        // pick two random numbers and swap - memArr.length times
+        tmp1 = Math.floor(Math.random() * memArr.length);
+        tmp2 = Math.floor(Math.random() * memArr.length);
+        [ memArr[tmp1], memArr[tmp2] ] = [ memArr[tmp2], memArr[tmp1] ];
+    }
+    return memArr;
+};
+
+// Organize in 'n' groups
+const Organize = n => {
+    const OB = Randomize(OB_members);
+    const YB = Randomize(YB_members);
+    ans = [];
+
+    // Initialize Answer array
+    for(var i = 1; i <= n; i++){
+        ans.push( [`< Group ${i} >`] );
+    }
+
+    // Push members
+    OB.forEach( mem => ans[OB.indexOf(mem)%n].push(mem) );
+    YB.forEach( mem => ans[YB.indexOf(mem)%n].push(mem) );
+
+    return ans;
+}
+
+console.log(Organize(5));
