72c11524cec858ce589e72dd45074266c641e099
yxxshin <samshin3910@snu.ac.kr>
2020-11-01 14:41:32 +0900

    Finished 3rd_seminar_hw_level3

---
+++ homework/3rd_seminar_hw/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+// Connection
+router.use('/users', require('./users'));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
