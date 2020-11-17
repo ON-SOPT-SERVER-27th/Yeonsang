9587f14694aea97457a394a6478439b28dbefe11
yxxshin <samshin3910@snu.ac.kr>
2020-11-18 03:11:11 +0900

    Modified 4th-seminar/routes/users/index.js

--- 4th-seminar/routes/users/index.js
+++ 4th-seminar/routes/users/index.js
@@ -7,9 +7,7 @@ const statusCode = require('../../modules/statusCode');
 const {
     User
 } = require('../../models');
-const {
-    stat
-} = require('fs');
+
 router.post('/signup', async (req, res) => {
     const {
         email,
@@ -49,8 +47,6 @@ router.post('/signup', async (req, res) => {
             salt: salt,
         });
 
-        console.log(user.userName);
-        console.log(user);
         //7. status: 200 message: SING_UP_SUCCESS, data: id, email, userName 반환! (비밀번호, salt 반환 금지!!)
         return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, {
             id: user.id,
@@ -65,13 +61,52 @@ router.post('/signup', async (req, res) => {
 router.post('/signin', async (req, res) => {
     const {
         email,
-        password
+        password,
     } = req.body; // 1. req.body에서 데이터 가져오기
+
     //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
-    //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
-    //4. 비밀번호 확인하기 - 로그인할 email의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
-    // 일치하지 않으면 Miss Match password 반환
-    //5. status: 200 ,message: SIGN_IN_SUCCESS, data: id, email, userName 반환
+    if (!email || !password) {
+        console.log('필요한 값이 없습니다');
+        return res.status()
+    }
+
+    try {
+        //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
+        const alreadyEmail = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!alreadyEmail) {
+            console.log('없는 이메일입니다');
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+        }
+
+        //4. 비밀번호 확인하기 - 로그인할 email의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
+        // 일치하지 않으면 Miss Match password 반환
+        const {
+            email,
+            password: savedPassword,
+            userName,
+            salt,
+        } = alreadyEmail;
+        
+        const inputPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
+
+        if(inputPassword !== savedPassword) {
+            console.log('비밀번호가 일치하지 않습니다');
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
+        }
+
+        //5. status: 200 ,message: SIGN_IN_SUCCESS, data: id, email, userName 반환
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, { id, email, userName }));
+    } catch (error) {
+        console.error(error);
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_IN_FAIL));
+    }
+
+
 })
 router.get('/', async (req, res) => {
     //1. 모든 사용자 정보 (id, email, userName ) 리스폰스!
@@ -87,6 +122,7 @@ router.get('/', async (req, res) => {
         return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusc.INTERNAL_SERVER_ERROR, responseMessage.READ_USER_ALL_FAIL));
     }
 })
+
 router.get('/:id', async (req, res) => {
     //1. parameter로 id값을 받아온다! (id값은 인덱스값)
     const {
@@ -105,6 +141,7 @@ router.get('/:id', async (req, res) => {
             console.log('존재하지 않는 아이디 입니다.');
             return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
         }
+        
         //3. status:200 message: READ_USER_SUCCESS, id, email, userName 반환
         return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_USER_SUCCESS, user));
     } catch (error) {
