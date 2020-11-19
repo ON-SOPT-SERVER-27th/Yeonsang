208a0d1bec119543f0afa8ddc7853d0815480646
yxxshin <samshin3910@snu.ac.kr>
2020-11-20 04:10:30 +0900

    Modified userController.js & routes/users/index.js

--- 5th-seminar/controller/userController.js
+++ 5th-seminar/controller/userController.js
@@ -2,27 +2,31 @@ const crypto = require('crypto');
 const util = require('../modules/util');
 const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
-const { User } = require('../models');
-const { userService } = require('../service');
+const {
+    User
+} = require('../models');
+const {
+    userService
+} = require('../service');
 
 
 module.exports = {
-    signup : async (req, res) => {
+    signup: async (req, res) => {
         const {
             email,
             password,
             userName
         } = req.body; // 1. req.body에서 데이터 가져오기
-    
+
         //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
         if (!email || !password || !userName) {
             console.log('필요한 값이 없습니다!');
             return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
         }
-    
+
         try {
             //3. 존재하는 이메일인지 확인하기. 이미 존재하는 이메일면 ALREADY ID 반환
-            
+
             /*
             const alreadyEmail = await User.findOne({
                 where: {
@@ -30,14 +34,14 @@ module.exports = {
                 }
             });
             */
-            
+
             const alreadyEmail = await userService.readOneEmail(email);
-    
+
             if (alreadyEmail) {
                 console.log('이미 존재하는 이메일입니다.');
                 return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
             }
-            
+
             /*
             //4. salt 생성 
             const salt = crypto.randomBytes(64).toString('base64');
@@ -52,9 +56,9 @@ module.exports = {
                 salt: salt,
             });
             */
-            
+
             const user = await userService.signup(email, userName, password);
-    
+
             console.log(user.userName);
             console.log(user);
             //7. status: 200 message: SING_UP_SUCCESS, data: id, email, userName 반환! (비밀번호, salt 반환 금지!!)
@@ -69,18 +73,18 @@ module.exports = {
         }
     },
 
-    signin : async (req, res) => {
+    signin: async (req, res) => {
         const {
             email,
             password,
         } = req.body; // 1. req.body에서 데이터 가져오기
-    
+
         //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
         if (!email || !password) {
             console.log('필요한 값이 없습니다');
             return res.status()
         }
-    
+
         try {
             //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
             const alreadyEmail = await User.findOne({
@@ -88,12 +92,12 @@ module.exports = {
                     email: email,
                 },
             });
-    
+
             if (!alreadyEmail) {
                 console.log('없는 이메일입니다');
                 return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
             }
-    
+
             //4. 비밀번호 확인하기 - 로그인할 email의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
             // 일치하지 않으면 Miss Match password 반환
             const {
@@ -102,25 +106,29 @@ module.exports = {
                 userName,
                 salt,
             } = alreadyEmail;
-            
+
             const inputPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
-    
-            if(inputPassword !== savedPassword) {
+
+            if (inputPassword !== savedPassword) {
                 console.log('비밀번호가 일치하지 않습니다');
                 return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
             }
-    
+
             //5. status: 200 ,message: SIGN_IN_SUCCESS, data: id, email, userName 반환
-            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, { id, email, userName }));
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, {
+                id,
+                email,
+                userName
+            }));
         } catch (error) {
             console.error(error);
             return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_IN_FAIL));
         }
-    
-    
+
+
     },
 
-    readAll : async (req, res) => {
+    readAll: async (req, res) => {
         //1. 모든 사용자 정보 (id, email, userName ) 리스폰스!
         // status: 200, message: READ_USER_ALL_SUCCESS, data: id, email, userName 반환
         try {
@@ -135,7 +143,7 @@ module.exports = {
         }
     },
 
-    readOne : async (req, res) => {
+    readOne: async (req, res) => {
         //1. parameter로 id값을 받아온다! (id값은 인덱스값)
         const {
             id
@@ -148,7 +156,7 @@ module.exports = {
                 },
                 attributes: ['id', 'email', 'userName'],
             });
-    
+
             if (!user) {
                 console.log('존재하지 않는 아이디 입니다.');
                 return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
@@ -158,6 +166,85 @@ module.exports = {
         } catch (error) {
             console.error(error);
             return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.READ_USER_FAIL));
+        }
+    },
+
+    updateUser: async (req, res) => {
+        // 1. parameter로 id 값 받아오고, body로 값 받아옴
+        const {
+            id
+        } = req.params;
+
+        const {
+            email,
+            password,
+            userName
+        } = req.body;
+
+        try {
+            // 2. id값이 유효한지 체크! 존재하지 않는 유저면 NO_USER 반환
+            const user = await User.findOne({
+                where: {
+                    id: id,
+                },
+                attributes: ['id'],
+            });
+
+            if (!user) {
+                console.log('존재하지 않는 아이디 입니다.');
+                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+            }
+
+            // 3. 사용자가 있다면, body의 값으로 수정
+            const salt = crypto.randomBytes(64).toString('base64');
+            const newPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
+
+            const update = await User.update({
+                email,
+                password: newPassword,
+                userName,
+                salt,
+            }, {
+                where: {
+                    id: id,
+                }
+            });
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_UPDATE_SUCCESS));
+        } catch (error) {
+            console.error(error);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+        }
+    },
+
+    deleteUser: async (req, res) => {
+        // 1. parameter 로 id 값 받아오기
+        const {
+            id
+        } = req.params;
+
+        try {
+            // 2. id값이 유효한지 체크! 존재하지 않는 유저면 NO_USER 반환
+            const user = await User.findOne({
+                where: {
+                    id: id,
+                }
+            });
+
+            if (!user) {
+                console.log('존재하지 않는 아이디 입니다');
+                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+            }
+
+            // 3. 사용자가 있다면, 삭제
+            const del = await User.destroy({
+                where: {
+                    id
+                }
+            });
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_DELETE_SUCCESS));
+        } catch (error) {
+            console.error(error);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
         }
     }
 }
--- 5th-seminar/routes/users/index.js
+++ 5th-seminar/routes/users/index.js
@@ -6,5 +6,7 @@ router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
 router.get('/', userController.readAll);
 router.get('/:id', userController.readOne);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
