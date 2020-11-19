c7126e759bfc5b63e7489cb77abb951954e05073
yxxshin <samshin3910@snu.ac.kr>
2020-11-20 04:26:33 +0900

    Finished 5th_seminar_hw_level3

--- homework/5th_seminar_hw/controller/userController.js
+++ homework/5th_seminar_hw/controller/userController.js
@@ -3,11 +3,13 @@ const util = require('../modules/util');
 const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
 const {
-    User
+    User, Post
 } = require('../models');
 const {
     userService
 } = require('../service');
+const { stat } = require('fs');
+const { success } = require('../modules/util');
 
 
 module.exports = {
@@ -245,6 +247,36 @@ module.exports = {
         } catch (error) {
             console.error(error);
             return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+        }
+    },
+
+    readUserInfo: async (req, res) => {
+        // user의 email, userName, user가 쓴 게시글 모두 불러오기
+        const {
+            id
+        } = req.params;
+
+        try {
+            const user = await User.findAll( {
+                where: {
+                    id
+                },
+                attributes: ['id', 'email', 'userName'],
+                include: [{
+                    model: Post,
+                    attributes: { exclude: ['UserId']}
+                }]
+            })
+
+            if(!user) {
+                console.log('존재하지 않는 아이디입니다');
+                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+            }
+
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_USER_SUCCESS, user));
+        } catch (err) {
+            console.log(err);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.READ_USER_FAIL));
         }
     }
 }
--- homework/5th_seminar_hw/routes/users/index.js
+++ homework/5th_seminar_hw/routes/users/index.js
@@ -5,7 +5,8 @@ const userController = require('../../controller/userController');
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
 router.get('/', userController.readAll);
-router.get('/:id', userController.readOne);
+// router.get('/:id', userController.readOne);
+router.get('/:id', userController.readUserInfo);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
