c6ac1b8a4b6b8fa4135445dbc42580ab55e9531d
yxxshin <samshin3910@snu.ac.kr>
2020-11-20 04:00:14 +0900

    Modified controller/userController.js

--- homework/5th_seminar_hw/controller/userController.js
+++ homework/5th_seminar_hw/controller/userController.js
@@ -70,35 +70,54 @@ module.exports = {
     },
 
     signin : async (req, res) => {
-        const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
-        //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
-        if( !id || !password ){
-            console.log('id 혹은 비밀번호가 입력되지 않았습니다');
-            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+        const {
+            email,
+            password,
+        } = req.body; // 1. req.body에서 데이터 가져오기
+    
+        //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
+        if (!email || !password) {
+            console.log('필요한 값이 없습니다');
+            return res.status()
         }
     
-        //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
-        const mem = usersDB.filter(mem => mem.id == id);
-        if(mem.length === 0) {
-            console.log('id가 존재하지 않습니다.');
-            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+        try {
+            //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
+            const alreadyEmail = await User.findOne({
+                where: {
+                    email: email,
+                },
+            });
+    
+            if (!alreadyEmail) {
+                console.log('없는 이메일입니다');
+                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+            }
+    
+            //4. 비밀번호 확인하기 - 로그인할 email의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
+            // 일치하지 않으면 Miss Match password 반환
+            const {
+                id,
+                password: savedPassword,
+                userName,
+                salt,
+            } = alreadyEmail;
+            
+            const inputPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
+    
+            if(inputPassword !== savedPassword) {
+                console.log('비밀번호가 일치하지 않습니다');
+                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
+            }
+    
+            //5. status: 200 ,message: SIGN_IN_SUCCESS, data: id, email, userName 반환
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, { id, email, userName }));
+        } catch (error) {
+            console.error(error);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_IN_FAIL));
         }
     
-        //4. 비밀번호 확인하기 - 로그인할 id의 salt를 DB에서 가져와서  사용자가 request로 보낸 password와
-        //   암호화를 한후 디비에 저장되어있는 password와 일치하면 true일치하지 않으면 Miss Match password 반환
-        //5. status: 200 ,message: SIGNIN SUCCESS, data: id 반환 (비밀번호, salt 반환 금지!!)
-        const salt = mem[0].salt;
-        const encrypted_pw = mem[0].password;
-        crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err,key) => {
-            if(key.toString('base64') === encrypted_pw) {
-                console.log('로그인 성공!');
-                return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, id));
-            }
-            else {
-                console.log('로그인 실패');
-                return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.SIGN_IN_FAIL));
-            }
-        })
+    
     },
 
     readAll : async (req, res) => {
